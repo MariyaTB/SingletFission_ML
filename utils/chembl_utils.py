5f1dcf92c4cbd1330a877dc2d600d57a5c4d6837
christy726 <christygeorge726@gmail.com>
2025-02-19 11:41:15 +0530

    Edits

--- utils/chembl_utils.py
+++ utils/chembl_utils.py
@@ -7,11 +7,9 @@ def get_chembl_smiles(limit=50000):
     molecule = new_client.molecule
     logging.info(f"Attempting to retrieve up to {limit} molecules from ChEMBL...")
 
-    # Retrieve molecules with a preferred name
     molecules = molecule.filter(pref_name__isnull=False)[:limit]
     logging.info(f"Retrieved {len(molecules)} molecules with a preferred name.")
 
-    # Filter molecules with valid canonical SMILES
     valid_smiles = [mol['molecule_structures']['canonical_smiles']
                     for mol in molecules
                     if mol and 'molecule_structures' in mol and mol['molecule_structures'] is not None and 'canonical_smiles' in mol['molecule_structures']]
