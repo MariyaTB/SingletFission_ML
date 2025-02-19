147a4fff907ca69b2c4e8f5e2c31144d3186f403
christy726 <christygeorge726@gmail.com>
2025-02-19 11:00:27 +0530

    refactor data augmentation process and update output CSV for augmented SMILES

--- Data_Augmentation/Augment.py
+++ Data_Augmentation/Augment.py
@@ -87,6 +87,6 @@ augmented_smiles = augmentation_by_fragment(core_smiles_list,700000)
 filtered_augmented_smiles = filter_small_molecules(augmented_smiles)
 
 augmented_df = pd.DataFrame(filtered_augmented_smiles, columns=['SMILES'])
-augmented_df.to_csv(r'D:\My_Computer\Meh!!!!!!!\workspace\VIT_LAB\SingletFission_ML\data\augmented_25Atoms_19_02_2025.csv', index=False)
+augmented_df.to_csv(r'D:\My_Computer\Meh!!!!!!!\workspace\VIT_LAB\SingletFission_ML\data\augmented.csv', index=False)
 
-print(f"Generated {len(filtered_augmented_smiles)} augmented SMILES with up to  25 atoms.")
+print(f"Generated {len(filtered_augmented_smiles)} augmented SMILES with up to 30 atoms.")
