fe35890cf1f06340dfbe809c49d8be64bf675e91
christy726 <christygeorge726@gmail.com>
2025-02-21 11:49:39 +0530

    Add new SMILES and corresponding SMI files for multiple families in data augmentation for better data augmentation

---
+++ Data_Augmentation/smiles.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+from rdkit import Chem
+
+smi_file_path = os.path.join('Data_Augmentation', 'families', 'family16.smi')
+
+with open(smi_file_path, 'r') as file:
+    lines = file.readlines()
+
+molecules = [Chem.MolFromSmiles(line.strip().split()[0]) for line in lines]
+smiles_list = [Chem.MolToSmiles(mol) if mol else 'Invalid' for mol in molecules]
+
+df = pd.DataFrame({'SMILES': smiles_list})
+
+csv_file_path = os.path.join('Data_Augmentation', 'family16.csv')
+df.to_csv(csv_file_path, index=False)
+
+print(f'SMILES data saved to {csv_file_path}')
