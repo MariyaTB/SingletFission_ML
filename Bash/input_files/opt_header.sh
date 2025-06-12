56ba9dff87183d3e1ca9f3dcc151deee760ba966
christy726 <christygeorge726@gmail.com>
2025-06-12 14:28:10 +0530

    Clean up opt_header.sh by removing commented-out code and ensuring proper script termination

--- Bash/input_files/opt_header.sh
+++ Bash/input_files/opt_header.sh
@@ -8,9 +8,3 @@ for file in *; do
         tail -n +4 "$file"
     } > "$file.tmp" && mv "$file.tmp" "$file"
 done
-
-# for file in *; doAdd commentMore actions
-#     [[ "$file" == "$(basename "$0")" ]] && continue
-#     [[ ! -f "$file" ]] && continue
-#     sed -i '1i# opt b3lyp/6-31g' "$file"
-# done
