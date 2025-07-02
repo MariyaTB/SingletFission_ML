d6a64938f25a1ea63c6d6fd90c4e4a7b7382d387
christy726 <christygeorge726@gmail.com>
2025-07-02 17:07:23 +0530

    Implement code changes to enhance functionality and improve performance

--- duplicate_data_clean/data_cleaning.py
+++ duplicate_data_clean/data_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df_a = pd.read_csv('SF_Files/valid_unique_smiles_28_02_2025.csv')
-df_b = pd.read_csv('SF_Files/valid_unique_smiles_01_04_2025.csv')
+df_a = pd.read_csv('duplicate_data_clean/valid_unique_smiles_28_02_2025.csv')
+df_b = pd.read_csv('duplicate_data_clean/valid_unique_smiles_01_04_2025.csv')
 
 smiles_col = df_a.columns[1]
 
@@ -10,9 +10,9 @@ print(f"Number of duplicates: {len(duplicates)}")
 
 df_a_filtered = df_a[~df_a[smiles_col].isin(df_b[smiles_col])]
 
-result = pd.concat([df_b, df_a_filtered], ignore_index=True)
+result = pd.concat([df_a_filtered, df_b], ignore_index=True)
 
-result.to_csv('SF_Files/valid_unique_smiles_28_02_2025.csv', index=False)
+result.to_csv('duplicate_data_clean/valid_unique_smiles_28_02_2025.csv', index=False)
 
 
 # import pandas as pd
